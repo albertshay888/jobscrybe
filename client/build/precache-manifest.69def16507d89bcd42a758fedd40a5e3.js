705dd2342ab111514ef3e6849ad2fef04fa5da0d
ashay <albertshay888@gmail.com>
2019-03-13 15:58:30 -0700

    deploy

---
+++ client/build/precache-manifest.69def16507d89bcd42a758fedd40a5e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b18ae67916fd39090673",
+    "url": "/static/css/main.c132f6be.chunk.css"
+  },
+  {
+    "revision": "b18ae67916fd39090673",
+    "url": "/static/js/main.b18ae679.chunk.js"
+  },
+  {
+    "revision": "7aeceb793c592714c06a",
+    "url": "/static/js/1.7aeceb79.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7fd6f3b65191561e611b686f23fafd63",
+    "url": "/static/media/logo.7fd6f3b6.svg"
+  },
+  {
+    "revision": "82c5a1dc12cfa0dd8cb78e4c08355d60",
+    "url": "/index.html"
+  }
+];
