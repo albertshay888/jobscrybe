3ac7ad4af3610b5d1962af7e01931f9bc2699afa
ashay <albertshay888@gmail.com>
2019-03-19 21:30:05 -0700

    algorithm implemented

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,9 +5,9 @@ export default {
     getJobs: function(q) {
       // console.log(q);
       // console.log(loc);
-      return (axios.get(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${q.search}&location=${q.location}`));
+      // return (axios.get(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${q.search}&location=${q.location}`));
       //** use the code below when pushing to heroku */
-      // return (axios.get(`https://quiet-badlands-79780.herokuapp.com/https://jobs.github.com/positions.json?description=${q.search}&location=${q.location}`));
+      return (axios.get(`https://quiet-badlands-79780.herokuapp.com/https://jobs.github.com/positions.json?description=${q.search}&location=${q.location}`));
     },
     // Gets all saved jobs
     getSavedJobs: function() {
@@ -127,9 +127,8 @@ export default {
           }
           percent = parseFloat((correct/words)*100).toFixed(2);
     
-      final.push("JOB DESCRIPTION MATCH RATE: " + percent + "%! ");
-      final.push("RESUME WORD COUNT: " + resumeWordCount + " words");
-      final.push("KEYWORD COMPAIRISON RESULT:");
+      final.push(percent);
+      final.push(resumeWordCount);
       final.push(total);
       return final;
       }
